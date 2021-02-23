d6d33e99696e808cb89a4af3ecf3406281d9d0f2
wherrr <73908711+wherrr@users.noreply.github.com>
2021-02-22 17:13:18 -0800

    Add coin purse placeholder

---
+++ src/main/java/io/github/wherrr/coincollecting/CoinPurseItem.java
@@ -0,0 +1,13 @@
+package io.github.wherrr.coincollecting;
+
+import net.minecraft.item.Item;
+
+public class CoinPurseItem extends Item
+{
+	public CoinPurseItem(Settings settings)
+	{
+		super(settings);
+	}
+	
+	
+}
--- src/main/java/io/github/wherrr/coincollecting/ModItems.java
+++ src/main/java/io/github/wherrr/coincollecting/ModItems.java
@@ -8,13 +8,18 @@ import net.minecraft.util.registry.Registry;
 public class ModItems
 {
 	public static final Item COIN = new Item(new FabricItemSettings().group(Main.COIN_COLLECTING_TAB).maxCount(1));
+	public static final CoinPurseItem COIN_PURSE = new CoinPurseItem(new FabricItemSettings().group(Main.COIN_COLLECTING_TAB).maxCount(1));
 	
 	public static <T extends Item> void registerItem(String identifierPath, T item)
 	{
 		Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, identifierPath), item);
 	}
+	
 	public static void registerItems()
 	{
 		registerItem("coin", COIN);
+		registerItem("coin_purse", COIN_PURSE);
 	}
+	
+	
 }
