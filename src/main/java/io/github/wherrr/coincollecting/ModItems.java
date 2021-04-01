d15cf10e1852d3a405b9087fed0aebcaf0447bf5
wherrr <73908711+wherrr@users.noreply.github.com>
2021-04-01 14:41:33 -0700

    Add coin drops for overworld huminoid hostiles

--- src/main/java/io/github/wherrr/coincollecting/ModItems.java
+++ src/main/java/io/github/wherrr/coincollecting/ModItems.java
@@ -55,9 +55,42 @@ public class ModItems
 	
 	public static void registerLootTables()
 	{
+		final Identifier CREEPER_LOOT_TABLE_ID = new Identifier("minecraft","entities/creeper");
+		final Identifier DROWNED_LOOT_TABLE_ID = new Identifier("minecraft","entities/drowned");
+		final Identifier ENDERMAN_LOOT_TABLE_ID = new Identifier("minecraft","entities/enderman");
+		final Identifier EVOKER_LOOT_TABLE_ID = new Identifier("minecraft","entities/evoker");
+		final Identifier HUSK_LOOT_TABLE_ID = new Identifier("minecraft","entities/husk");
+		final Identifier PILLAGER_LOOT_TABLE_ID = new Identifier("minecraft","entities/pillager");
+		final Identifier SKELETON_LOOT_TABLE_ID = new Identifier("minecraft","entities/skeleton");
+		final Identifier STRAY_LOOT_TABLE_ID = new Identifier("minecraft","entities/stray");
+		final Identifier VINDICATOR_LOOT_TABLE_ID = new Identifier("minecraft","entities/vindicator");
+		final Identifier WITCH_LOOT_TABLE_ID = new Identifier("minecraft","entities/witch");
 		final Identifier ZOMBIE_LOOT_TABLE_ID = new Identifier("minecraft","entities/zombie");
-		final Identifier ZOMBIE_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID, "shared/zombie_overworld_coin");
+		final Identifier ZOMBIE_VILLAGER_LOOT_TABLE_ID = new Identifier("minecraft","entities/zombie_villager");
+		final Identifier CREEPER_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/creeper_overworld_coin");
+		final Identifier DROWNED_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/drowned_overworld_coin");
+		final Identifier ENDERMAN_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/enderman_overworld_coin");
+		final Identifier EVOKER_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/evoker_overworld_coin");
+		final Identifier HUSK_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/husk_overworld_coin");
+		final Identifier PILLAGER_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/pillager_overworld_coin");
+		final Identifier SKELETON_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/skeleton_overworld_coin");
+		final Identifier STRAY_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/stray_overworld_coin");
+		final Identifier VINDICATOR_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/vindicator_overworld_coin");
+		final Identifier WITCH_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/witch_overworld_coin");
+		final Identifier ZOMBIE_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/zombie_overworld_coin");
+		final Identifier ZOMBIE_VILLAGER_OVERWORLD_COIN_LOOT_TABLE_ID = new Identifier(Main.MOD_ID,"shared/zombie_villager_overworld_coin");
 		
+		registerCoinLootTable(CREEPER_LOOT_TABLE_ID, CREEPER_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(DROWNED_LOOT_TABLE_ID, DROWNED_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(ENDERMAN_LOOT_TABLE_ID, ENDERMAN_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(EVOKER_LOOT_TABLE_ID, EVOKER_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(HUSK_LOOT_TABLE_ID, HUSK_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(PILLAGER_LOOT_TABLE_ID, PILLAGER_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(SKELETON_LOOT_TABLE_ID, SKELETON_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(STRAY_LOOT_TABLE_ID, STRAY_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(VINDICATOR_LOOT_TABLE_ID, VINDICATOR_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(WITCH_LOOT_TABLE_ID, WITCH_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
 		registerCoinLootTable(ZOMBIE_LOOT_TABLE_ID, ZOMBIE_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
+		registerCoinLootTable(ZOMBIE_VILLAGER_LOOT_TABLE_ID, ZOMBIE_VILLAGER_OVERWORLD_COIN_LOOT_TABLE_ID, OVERWORLD_COIN, CoinMints.COIN_MINT_OVERWORLD);
 	}
 }
